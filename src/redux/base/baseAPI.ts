5f1aef922bc0d762dcb578c2de418c6e978f1f02
mdbayzid2025 <ssmdbayzid23@gmail.com>
2026-03-09 15:43:05 +0600

    add ip

--- src/redux/base/baseAPI.ts
+++ src/redux/base/baseAPI.ts
@@ -6,7 +6,7 @@ export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
 
-    baseUrl: "https://rakibur5000.binarybards.online/api/v1",   
+    baseUrl: "https://api.investors-hub.co.za/api/v1",   
     // baseUrl: "http://10.10.7.48:5000/api/v1",   
     prepareHeaders: (headers) => {      
       const token = Cookies.get("accessToken");
@@ -22,5 +22,5 @@ export const baseApi = createApi({
   tagTypes: ["user", "notifications", "admin", "category", "slider", "faqs", "withdrawal", "planner", "stock", "request", "cms", "brief",],
 });
 
-export const imageUrl = "https://rakibur5000.binarybards.online";
+export const imageUrl = "https://api.investors-hub.co.za";
 // export const imageUrl = "http://10.10.7.48:5000";
